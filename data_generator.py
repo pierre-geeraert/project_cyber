291734aa2900a599226f21883978477a4d2f165c
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2020-04-02 02:17:49 +0200

    find a little solution but it seems usefull

--- data_generator.py
+++ data_generator.py
@@ -9,25 +9,25 @@ def writeFile(text):
     f.write(text+"\n")
 
 def word_generator(number_of_letter):
-    word_list_temp=[]
+    word_list_temp=[""]*number_of_letter
 
     for id_column in range(0,number_of_letter):
         print("id_column"+str(id_column))
-        word_list_temp.insert(id_column, string.ascii_lowercase[id_letter])
+        #word_list_temp.insert(id_column, string.ascii_lowercase[id_letter])
         for id_letter in range(0, 26):
-            var_letter = string.ascii_lowercase[id_letter]
+            #var_letter = string.ascii_lowercase[id_letter]
             #word_list_temp.insert(id_column, string.ascii_lowercase[id_letter])
-            #word_list_temp[id_column]=string.ascii_lowercase[id_letter]
+            word_list_temp[(number_of_letter-1)-id_column]=string.ascii_lowercase[id_letter]
             print(word_list_temp)
 
 
-#word_generator(2)
+word_generator(2)
 
-number=1000
-for x in range(0,number):
+#number=1000
+#for x in range(0,number):
     #print(x)
-    liste=["1","2","3"]
+#    liste=["1","2","3"]
 #    print(len(str(x)))
-    liste[len(str(x))]=string.ascii_lowercase[1]
-    liste[1] = string.ascii_lowercase[list(str(x)[1])]
+#    liste[len(str(x))]=string.ascii_lowercase[1]
+#    liste[1] = string.ascii_lowercase[list(str(x)[1])]
     #print(liste)
