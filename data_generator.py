348d1570b7e6fac61d54481840e00558ca7cd929
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2020-04-02 02:29:25 +0200

    maybe find a solution

--- data_generator.py
+++ data_generator.py
@@ -13,21 +13,14 @@ def word_generator(number_of_letter):
 
     for id_column in range(0,number_of_letter):
         print("id_column"+str(id_column))
-        #word_list_temp.insert(id_column, string.ascii_lowercase[id_letter])
-        for id_letter in range(0, 26):
-            #var_letter = string.ascii_lowercase[id_letter]
-            #word_list_temp.insert(id_column, string.ascii_lowercase[id_letter])
-            word_list_temp[(number_of_letter-1)-id_column]=string.ascii_lowercase[id_letter]
-            print(word_list_temp)
+        for id_letter_2 in range(0,26):
+            word_list_temp[(number_of_letter-1)-id_column]=string.ascii_lowercase[id_letter_2]
+            for id_letter in range(0, 26):
+                word_list_temp[(number_of_letter-1)]=string.ascii_lowercase[id_letter]
+                print(word_list_temp)
+                writeFile(str(word_list_temp))
 
 
-word_generator(2)
+word_generator(3)
 
-#number=1000
-#for x in range(0,number):
-    #print(x)
-#    liste=["1","2","3"]
-#    print(len(str(x)))
-#    liste[len(str(x))]=string.ascii_lowercase[1]
-#    liste[1] = string.ascii_lowercase[list(str(x)[1])]
-    #print(liste)
+
